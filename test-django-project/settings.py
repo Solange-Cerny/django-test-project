3dcb07f2ade6905dd613b8b386087fc447d69285
Solange <sol.cer.87@gmail.com>
2019-07-28 12:22:10 +0100

    STATIC_ROOT

--- test-django-project/settings.py
+++ test-django-project/settings.py
@@ -113,7 +113,7 @@ STATIC_URL = '/static/'
 #STATIC_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep) + ['static']))
 #STATIC_ROOT = 'D:/home/site/wwwroot/static'
 
-STATIC_ROOT= os.path.join(BASE_DIR,'static-files')
+STATIC_ROOT= os.path.join(BASE_DIR,'static')
 
 STATICFILES_DIRS = [
     'D:/home/site/wwwroot/static',

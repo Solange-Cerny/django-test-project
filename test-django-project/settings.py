4b8e34d7531d095fa9fd7b5491362cee3904bc24
Solange <sol.cer.87@gmail.com>
2019-07-28 11:49:14 +0100

    STATIC_ROOT

--- test-django-project/settings.py
+++ test-django-project/settings.py
@@ -110,5 +110,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 #STATIC_URL = '/static/'
+#STATIC_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep) + ['static']))
 STATIC_URL = '/app/static/'
-STATIC_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep) + ['static']))
+STATIC_ROOT = 'D:/home/site/wwwroot/app/static'
